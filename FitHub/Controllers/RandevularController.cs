010d9d20b14e2848f4770f4d8293345a8d1a999f
Yağız Berke Su <yagizberkesuu@gmail.com>
2025-11-24 03:48:04 +0300

    bi kaç düzenleme

--- FitHub/Controllers/RandevularController.cs
+++ FitHub/Controllers/RandevularController.cs
@@ -23,6 +23,14 @@ namespace FitHub.Controllers
             return role == "Admin";
         }
 
+        // Dropdown listeleri doldurur
+        private async Task LoadDropdowns()
+        {
+            ViewData["UyeId"] = new SelectList(await _context.Uyeler.ToListAsync(), "Id", "Ad");
+            ViewData["EgitmenId"] = new SelectList(await _context.Egitmenler.ToListAsync(), "Id", "Ad");
+            ViewData["SalonId"] = new SelectList(await _context.Salonlar.ToListAsync(), "Id", "Ad");
+        }
+
         // GET: Randevular
         public async Task<IActionResult> Index()
         {
@@ -62,7 +70,6 @@ namespace FitHub.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Tarih,Saat,UyeId,EgitmenId,SalonId,Hizmet,Sure,Ucret,Onaylandi")] Randevu randevu)
         {
-            // Çakýþma kontrolü
             var baslangic = randevu.Tarih.Date.Add(TimeSpan.Parse(randevu.Saat));
             var bitis = baslangic.AddMinutes(randevu.Sure);
 
@@ -72,4 +79,97 @@ namespace FitHub.Controllers
                     r.Durum != "Ýptal" &&
                     r.Tarih.Date == randevu.Tarih.Date &&
                     (
-                        // Zaman aralýðý çak
+                        baslangic < r.Tarih.Date.Add(TimeSpan.Parse(r.Saat)).AddMinutes(r.Sure) &&
+                        bitis > r.Tarih.Date.Add(TimeSpan.Parse(r.Saat))
+                    )
+                )
+                .ToListAsync();
+
+            if (conflicts.Any())
+            {
+                ModelState.AddModelError("", "Seçilen eðitmen bu saatte baþka bir randevuya sahip.");
+                await LoadDropdowns();
+                return View(randevu);
+            }
+
+            randevu.Durum = "Beklemede";
+            _context.Add(randevu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Randevular/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var randevu = await _context.Randevular.FindAsync(id);
+            if (randevu == null) return NotFound();
+
+            await LoadDropdowns();
+            return View(randevu);
+        }
+
+        // POST: Randevular/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Tarih,Saat,UyeId,EgitmenId,SalonId,Hizmet,Sure,Ucret,Onaylandi,Durum")] Randevu randevu)
+        {
+            if (id != randevu.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(randevu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RandevuExists(randevu.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            await LoadDropdowns();
+            return View(randevu);
+        }
+
+        // GET: Randevular/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var randevu = await _context.Randevular
+                .Include(r => r.Uye)
+                .Include(r => r.Egitmen)
+                .Include(r => r.Salon)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (randevu == null) return NotFound();
+
+            return View(randevu);
+        }
+
+        // POST: Randevular/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var randevu = await _context.Randevular.FindAsync(id);
+            if (randevu != null)
+            {
+                _context.Randevular.Remove(randevu);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RandevuExists(int id)
+        {
+            return _context.Randevular.Any(e => e.Id == id);
+        }
+    }
+}
--- FitHub/Program.cs
+++ FitHub/Program.cs
@@ -1,11 +1,21 @@
 using FitHub.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http; // For session extensions
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<FitHubContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+// Add session services
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews(); // SADECE BURADA!
@@ -15,6 +25,9 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+// Enable session middleware
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
