1785aaddf886d32c7f99888c898f44964b0fe00d
Yağız Berke Su <yagizberkesuu@gmail.com>
2025-12-17 07:05:58 +0300

    commit

--- FitHub/Program.cs
+++ FitHub/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddDbContext<FitHubContext>(options =>
  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-// Add session services  a
+// Add session services  and configure session options
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
